c218f2500d6c85d0fe468cd81fc6c3ed0032c17a
Vagrant User <cj.fernanders1986@gmail.com>
2015-10-28 15:47:53 +0000

    Week 10 Warmup

---
+++ task_01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 01"""
+
+
+GRADE_DATA = {
+    "Luke Skywalker": {
+        "math": "B",
+        "etiquette": "B+",
+        "grammar": "B",
+        "gym": "A"
+        },
+    "Han Solo": {
+        "math": "A-",
+        "etiquette": "C-",
+        "grammar": "B",
+        "gym": "B"
+        },
+    "C-3PO": {
+        "math": "C",
+        "etiquette": "A+",
+        "grammar": "A",
+        "gym": "F"
+        },
+}
---
+++ task_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 02"""
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+BAND_NAMES = data.BANDS.keys()
