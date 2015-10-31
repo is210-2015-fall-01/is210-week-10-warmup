6744b261433445d73c1379c3a85e9122661311a3
Saulat Majid <solyraja@hotmail.com>
2015-10-31 01:16:01 +0000

    Revised task03.

--- task_03.py
+++ task_03.py
@@ -10,4 +10,4 @@ CORRECTED = data.BANDS.copy()
 CORRECTED['Dylan'] = {'Bob Dylan': ['vocals', 'guitar', 'harmonica']}
 
 del CORRECTED['Van Halen']['David Lee Roth']
-CORRECTED['Van Halen']['Sammy Hager'] = ['vocals']
+CORRECTED['Van Halen']['Sammy Hagar'] = ['vocals']
